d20db68443f00281fd63cfdea97f0970022fb10f
Lital Barak <litalbarak2@gmail.com>
2021-03-28 16:30:42 +0300

    mmmm i think im done

--- ex1.c
+++ ex1.c
@@ -5,14 +5,15 @@
 #include <string.h>
 #include <stdbool.h>
 
-struct HistoryCommand {
-   int  pid;
-   bool  is_running;
-   char  command_name[100];
-}; 
-
+struct HistoryCommand
+{
+    int pid;
+    char command_name[100];
+    bool is_job;
+};
 
 struct HistoryCommand commands_history[100];
+
 int command_history_index = 0;
 
 char **get_input(char *input)
@@ -66,6 +67,7 @@ void run_command(char **command)
     }
 
     pid = fork();
+    add_to_history(command[0], pid, isbackground);
 
     if (pid == 0)
     { /* Child */
@@ -82,31 +84,110 @@ void run_command(char **command)
         {
             waited = wait(&stat); /* stat can tell what happened */
         }
+        else
+        {
+        }
     }
 }
 
-void run_built_in_command(char **command){
-    if(strcmp(command[0],"cd") == 0){
+bool check_is_running(int pid)
+{
+    int stat;
+    int waited;
+    waited = waitpid(pid, &stat, WNOHANG);
+    if (waited == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void run_built_in_command(char **command)
+{
+    add_to_history(command[0], 0, false);
+    if (strcmp(command[0], "cd") == 0)
+    {
         cd_command(command[1]);
     }
+    if (strcmp(command[0], "history") == 0)
+    {
+        history_command();
+    }
+    if (strcmp(command[0], "jobs") == 0)
+    {
+        jobs_command();
+    }
 }
 
-void cd_command(char *dir){
-    if(strcmp(dir,"~") == 0){
-        strcpy(dir,getenv("HOME"));
+void cd_command(char *dir)
+{
+    if (strcmp(dir, "~") == 0)
+    {
+        strcpy(dir, getenv("HOME"));
     }
     chdir(dir);
 }
 
-void history_command(){
-    
+void jobs_command()
+{
+    for (int i = 0; i < command_history_index; i++)
+    {
+        int stat;
+        if (commands_history[i].is_job == true)
+        {
+            bool is_running = check_is_running(commands_history[i].pid);
+            if (is_running)
+            {
+                printf("%s\n", commands_history[i].command_name);
+            }
+        }
+    }
 }
 
-void add_to_history(char *command,int pid){
+void history_command()
+{
+    for (int i = 0; i < command_history_index; i++)
+    {
+        printf("%s ", commands_history[i].command_name);
+        int stat;
+        if (is_built_int_command(commands_history[i].command_name))
+        {
+            if (i == command_history_index - 1)
+            {
+                printf("RUNNING");
+            }
+            else
+            {
+                printf("DONE");
+            }
+        }
+        else
+        {
+            bool is_running = check_is_running(commands_history[i].pid);
+            if (is_running == true)
+            {
+                printf("RUNNING");
+            }
+            else
+            {
+                printf("DONE");
+            }
+        }
+        printf("\n");
+    }
+}
+
+void add_to_history(char *command, int pid, bool is_job)
+{
     struct HistoryCommand command_history;
-    command_history.is_running = true;
+    command_history.pid = pid;
+    command_history.is_job = is_job;
     strcpy(command_history.command_name, command);
     commands_history[command_history_index] = command_history;
+    command_history_index++;
 }
 
 int main(int argc, char *argv[])
@@ -124,7 +205,7 @@ int main(int argc, char *argv[])
         bool built_in_cmd = is_built_int_command(command[0]);
         if (built_in_cmd)
         {
-           run_built_in_command(command);
+            run_built_in_command(command);
         }
         else
         {
